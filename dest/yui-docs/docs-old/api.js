b12b81ed84193ae6527283174715ea2a9f556a5f
Alexei Zverev <me@azverev.com>
2016-11-20 20:15:22 +0300

    New YUI docs + iframe scrolling

---
+++ dest/yui-docs/docs-old/api.js
@@ -0,0 +1,28 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "nx",
+        "nx.Comparable",
+        "nx.Iterable",
+        "nx.Object",
+        "nx.Observable",
+        "nx.Serializable",
+        "nx.data.Collection",
+        "nx.data.Dictionary",
+        "nx.data.ObservableCollection",
+        "nx.data.ObservableDictionary",
+        "nx.data.ObservableObject",
+        "nx.data.Query",
+        "nx.data.SortedMap"
+    ],
+    "modules": [
+        "nx"
+    ],
+    "allModules": [
+        {
+            "displayName": "nx",
+            "name": "nx"
+        }
+    ]
+} };
+});
